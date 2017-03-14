932648c5a0288e7af4a4607396c918062d3308e7
Jim Kirkbride <james.kirkbride@gmail.com>
2017-03-13 21:35:36 -0400

    Committing after actually saving the files this time...

---
+++ endpoints.sh
@@ -0,0 +1,3 @@
+curl 'https://www.stirtrek.com/api3/speakers/getspeakers?year=2017' -H 'Accept-Encoding: gzip, deflate, sdch, br' -H 'Accept-Language: en-US,en;q=0.8' -H 'Accept: application/json, text/plain, */*' --compressed
+
+curl 'https://www.stirtrek.com/api3/Session/GetAcceptedSessionsByTimeslot?year=2017' -H 'Accept-Encoding: gzip, deflate, sdch, br' -H 'Accept-Language: en-US,en;q=0.8' -H 'Accept: application/json, text/plain, */*' --compressed
